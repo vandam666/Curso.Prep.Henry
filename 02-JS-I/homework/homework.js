ca32f3640ae3cab5a9b0bafe3f3a7f714da56162
github_vandam666 <vandam3320@gmail.com>
2022-01-10 22:25:57 -0500

    changelog

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "comentarios de entrada 1";
+const nuevaString = "comentarios de entrada";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
